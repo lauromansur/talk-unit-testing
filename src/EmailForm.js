adc3dc7b5fa57a35de1749550411d7e3254a9550
Lauro Nascimento <lauro.nascimento@hotmart.com>
2019-02-15 22:45:24 -0200

    Initial commit.

---
+++ src/EmailForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './EmailForm.css';
+
+class EmailForm extends Component {
+  render() {
+    return (
+      <div className="email-form">
+        <p>
+          Email form component.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default EmailForm;
