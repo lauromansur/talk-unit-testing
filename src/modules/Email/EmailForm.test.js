621df1eca42d4ba113099d647d78742822b28494
Lauro Nascimento <lauro.nascimento@hotmart.com>
2019-02-16 03:03:35 -0200

    Add enzyme unit tests.

--- src/modules/Email/EmailForm.test.js
+++ src/modules/Email/EmailForm.test.js
@@ -5,17 +5,50 @@ import { shallow } from 'enzyme';
 import EmailForm from './EmailForm';
 
 describe('EmailForm', () => {
-  test('renders component tree correctly (without enzyme)', () => {
-    const tree = renderer
-    .create(<EmailForm />)
-    .toJSON();
-
-    expect(tree).toMatchSnapshot();
+  describe('without enzyme', () => {
+    test('renders component tree correctly', () => {
+      const tree = renderer
+      .create(<EmailForm />)
+      .toJSON();
+  
+      expect(tree).toMatchSnapshot();
+    });
   });
 
-  test('renders component tree correctly (with enzyme)', () => {
-    const tree = shallow(<EmailForm />);
+  describe('with enzyme', () => {
+    test('renders component tree correctly', () => {
+      const tree = shallow(<EmailForm />);
+  
+      expect(tree).toMatchSnapshot();
+    });
 
-    expect(tree).toMatchSnapshot();
+    test('changing from field should persist in state', () => {
+      const tree = shallow(<EmailForm />);
+      const fromMockValue = 'John Doe';
+
+      expect(tree.state().from).toBe('');
+
+      tree.find('#from').simulate('change', {
+        target: {
+          name: 'from',
+          value: fromMockValue
+        }
+      });
+
+      expect(tree.state().from).toBe(fromMockValue);
+    });
+
+    test('sendEmail handler should be called on click event', () => {
+      const tree = shallow(<EmailForm />);
+
+      const spy = jest.spyOn(tree.instance(), 'sendEmail');
+      tree.instance().forceUpdate();
+      
+      tree.find('button[type="submit"]').simulate('click', {
+        preventDefault: jest.fn()
+      });
+
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
   });
 });
