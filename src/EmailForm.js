9be77018176369dd26649a9f789f375fb846d713
Lauro Nascimento <lauro.nascimento@hotmart.com>
2019-02-16 00:04:50 -0200

    Implement app interface.

--- src/EmailForm.js
+++ src/EmailForm.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 
+import { ReactComponent as PaperPlaneSolidIcon } from './icons/paper-plane-solid.svg';
+
 import './EmailForm.css';
 
 class EmailForm extends Component {
+  state = {
+    from: '',
+    to: '',
+    subject: '',
+    message: ''
+  }
+
   render() {
+    const { from, to, subject, message } = this.state;
+
     return (
-      <div className="email-form">
-        <p>
-          Email form component.
-        </p>
+      <div className="email__form">
+        <div className="form__control">
+          <label htmlFor="from">From:</label>
+          <input id="from" value={from} />
+        </div>
+
+        <div className="form__control">
+          <label htmlFor="to">To:</label>
+          <input id="to" value={to} />
+        </div>
+
+        <div className="form__control">
+          <label htmlFor="subject">Subject:</label>
+          <input id="subject" value={subject} />
+        </div>
+
+        <div className="form__control">
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" value={message} />
+        </div>
+
+        <div className="form__actions">
+          <button className="button__icon">
+            <span>Send</span>
+            <PaperPlaneSolidIcon />
+          </button>
+        </div>
       </div>
     );
   }
